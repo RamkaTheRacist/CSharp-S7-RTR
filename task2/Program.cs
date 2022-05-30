e2a0cded06df0ae2ae021dc1ad9a4ca54ed7e70a
Ramka <whereisourgod@gmail.com>
2022-05-31 01:57:04 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,47 @@
+﻿//Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+void FillArray(double[,] any)
+{
+    for(int i = 0; i < any.GetLength(0); i++)
+{
+    for(int j = 0; j < any.GetLength(1); j++)
+    {
+        any[i, j] = new Random().Next(0, 10);
+    }
+}
+}
+void PrintArray(double[,] any)
+{
+for(int i = 0; i < any.GetLength(0); i++) 
+{
+    for(int j = 0; j < any.GetLength(1); j++)
+    {
+        Console.Write($"{any[i, j]} ");
+    }
+Console.WriteLine();
+}
+}
+void ArithmeticMean(double[,] any, double[] array)
+{
+for(int i = 0; i < any.GetLength(0); i++)
+{
+    double result = 0;
+    for(int j = 0; j < any.GetLength(1); j++)
+    {
+        result = result + any[j, i];
+    }
+    array[i] = result / array.Length;
+    
+}
+}
+Console.WriteLine("How much rows in matrix?");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("How much columns in matrix?");
+int columns = int.Parse(Console.ReadLine());
+double[,] matrix = new double[rows, columns];
+double[] array = new double[rows];
+
+FillArray(matrix);
+PrintArray(matrix);
+ArithmeticMean(matrix, array);
+for(int i =0;i<array.Length;i++) Console.Write($"{array[i]} ");
+
